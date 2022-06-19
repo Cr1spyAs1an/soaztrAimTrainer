bc9ae3b4d9c9436bd0ceecc560919b0f3be89a59
furqa <furqa@LAPTOP-DP8FLFDJ.hitronhub.home>
2022-06-19 15:10:36 -0400

    Basically done the reaction time, look it over and make whatever changes you guys deem necessary.

--- soaztrAimTrainer/src/soaztrAimTrainer/Reactiontest.java
+++ soaztrAimTrainer/src/soaztrAimTrainer/Reactiontest.java
@@ -1,53 +1,74 @@
 package soaztrAimTrainer;
 
 import java.util.Random;
-
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
-import javafx.stage.Stage; 
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 public class Reactiontest extends Application {
-	public long random;
-	doube adasd = 0.0;
-	public boolean started = false;
-    boolean cheat;
-
-	
+	public Random random;
+	long start = 0;
+	int click = 0;
 	@Override
 	public void start(Stage stage) throws Exception {
-		VBox main = new VBox(20);
-		Scene scene = new Scene(main,500,500);
+		stage.setTitle("Hello World");
+	    Text text = new Text("After clicking start the background colour will change, try to click it as fast as you can"); 
+	    text.setFont(Font.font(null, FontWeight.BOLD, 15));    
+	    text.setFill(Color.BLACK);
+	    text.setX(50); 
+	    text.setY(50); 
+		Group root = new Group(text);
+		Scene scene = new Scene(root, 700, 500);
+		scene.setFill(Color.RED);
+		Button btn = new Button();
+		btn.setMinWidth(100);
+		btn.setMinHeight(50);
+		btn.setLayoutX(300);
+		btn.setLayoutY(210);
+		btn.setText("START");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			public void handle(ActionEvent event) {
+				try {
+					btn.setVisible(false);
+					Thread.sleep((long) (Math.random() * 5000));
+					long start1 = System.currentTimeMillis();
+					start = start1;
+
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				scene.setFill(Color.GREEN);
+			}		
+		});
+		EventHandler<MouseEvent> backGroundClick = new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				long end = System.currentTimeMillis();
+				click++;
+				if(click == 2) {
+				text.setX(250); 
+				text.setY(250);
+				text.setText("Your reaction time is " + (end - start) + " ms");
+				} 
+			}
+		};
+		
+		scene.addEventFilter(MouseEvent.MOUSE_CLICKED, backGroundClick);
+		
+		root.getChildren().add(btn);
 		stage.setScene(scene);
 		stage.show();
-		scene.setFill(Color.RED); 
-		
-		
-		/* the javafx scene doesnt run as long as you have this section of code below idfk why 
-		 * and i have on fucking clue if this code would even work mathew go crazy tryna fix it if you want
-		 */
-		while(!started) {     
-        long start = System.currentTimeMillis(); 
-        long end = System.currentTimeMillis();// Start timer
-        long randomTime = Math.round(Math.random() * 5000) + 1000;                     // Set random time
-        try {
-            Thread.sleep(random); 
-	        boolean notFirst = true;
-	        if (started) {              
-	            started = false;                                                    //end clock method
-					Object reactionTime = "Your Reaction Speed Is " + (end - (start + randomTime)) + " Milliseconds. Click to start again";
-	            }	
-        }
-        catch(InterruptedException e) {} 
-		}
-		}
-		
-		
-		
-	
+	}
 
 	public static void main(String[] args) {
 		launch(args);
@@ -55,4 +76,3 @@ public class Reactiontest extends Application {
 	}
 
 }
-
